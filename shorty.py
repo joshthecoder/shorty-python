2cc5198edee1069d31a47a31b9a889f3d779be4c
Josh Roesslein <jroesslein@gmail.com>
2009-07-24 02:33:35 -0500

    Added cli.gs service. Implemented shrink and expand methods.

--- shorty.py
+++ shorty.py
@@ -268,6 +268,31 @@ class Isgd(Service):
 
 isgd = Isgd()
 
+# cli.gs
+class Cligs(Service):
+
+    def __init__(self, apikey=None, appid=None):
+        self.apikey = apikey
+        self.appid = appid
+
+    def shrink(self, bigurl, title=None):
+        parameters = {'url': bigurl}
+        if title:
+            parameters['title'] = title
+        if self.apikey:
+            parameters['key'] = self.apikey
+        if self.appid:
+            parameters['appid'] = self.appid
+        resp = request('http://cli.gs/api/v1/cligs/create', parameters)
+        return resp.read()
+
+    def expand(self, tinyurl):
+        # TODO: debug this some more, not working properly
+        resp = request('http://cli.gs/api/v1/cligs/expand', {'clig': tinyurl})
+        return resp.read()
+
+cligs = Cligs()
+
 """Mapping of domain to service class"""
 services = {
     'sandbox': sandbox,
@@ -275,7 +300,8 @@ services = {
     'tr.im': trim,
     'urlborg.com': urlborg, 'ub0.cc': urlborg,
     'bit.ly': bitly,
-    'is.gd': isgd
+    'is.gd': isgd,
+    'cli.gs': cligs
 }
 
 """
