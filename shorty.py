6bb66072f260ea47cbf42e982b8c22edbffedfd1
Josh Roesslein <jroesslein@gmail.com>
2009-07-24 23:00:19 -0500

    Added digg.com service.

--- shorty.py
+++ shorty.py
@@ -322,6 +322,43 @@ class Tweetburner(Service):
 
 tweetburner = Tweetburner()
 
+# digg
+class Digg(Service):
+
+    def __init__(self, appkey=None):
+        self.appkey = appkey
+        self.itemid = None
+        self.view_count = None
+
+    def shrink(self, bigurl):
+        if not self.appkey:
+            raise ShortyError('Must set an appkey')
+        resp = request('http://services.digg.com/url/short/create',
+            {'url': bigurl, 'appkey': self.appkey, 'type': 'json'})
+        jdata = json.loads(resp.read())['shorturls'][0]
+        self.itemid = jdata['itemid']
+        self.view_count = jdata['view_count']
+        return str(jdata['short_url'])
+
+    def expand(self, tinyurl):
+        if self.appkey:
+            turl = urlparse(tinyurl)
+            if turl.netloc != 'digg.com' and turl.netloc != 'www.digg.com':
+                raise ShortyError('Not a valid digg url')
+            resp = request('http://services.digg.com/url/short/%s' % quote(
+                            turl.path.strip('/')),
+                            {'appkey': self.appkey, 'type': 'json'})
+            jdata = json.loads(resp.read())['shorturls'][0]
+            self.itemid = jdata['itemid']
+            self.view_count = jdata['view_count']
+            return str(jdata['link'])
+        else:
+            self.itemid = None
+            self.view_count = None
+            return get_redirect(tinyurl)
+
+digg = Digg()
+
 """Mapping of domain to service class"""
 services = {
     'sandbox': sandbox,
@@ -331,7 +368,8 @@ services = {
     'bit.ly': bitly,
     'is.gd': isgd,
     'cli.gs': cligs,
-    'tweetburner': tweetburner, 'twurl.nl': tweetburner
+    'tweetburner': tweetburner, 'twurl.nl': tweetburner,
+    'digg.com': digg
 }
 
 """
